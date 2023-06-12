e0f9ed2d5b5663881e7f898595b3ce6c2e206622
sophiazoi <133604009+sophiazoi@users.noreply.github.com>
2023-06-12 15:35:44 +0200

    updated

--- Advanced/index.js
+++ Advanced/index.js
@@ -1,66 +1,50 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const booksContainer = document.getElementById("books-container");
+const booksData = `[
+  {
+      "title": "JavaScript for Dummies",
+      "author": "Dummy Dumb Dumb",
+      "read": false,
+      "cover": "javascript_cover.jpg"
+  },
+  {
+      "title": "HTML & CSS Basics",
+      "author": "Coder McCoder",
+      "read": true,
+      "cover": "html_css_cover.jpg"
+  },
+  {
+      "title": "Python Crash Course",
+      "author": "Pythonista Pro",
+      "read": false,
+      "cover": "python_cover.jpg"
+  }
+]`;
 
-  // Retrieve books data from local storage or use an empty array if not available
-  const booksData = localStorage.getItem("books")
-    ? JSON.parse(localStorage.getItem("books"))
-    : [];
+const books = JSON.parse(booksData);
+const booksContainer = document.getElementById("books-container");
 
-  // Render the books on the page
-  function renderBooks() {
-    booksContainer.innerHTML = ""; // Clear the container before rendering
-
-    booksData.forEach((book) => {
-      // Create the book element
-      const bookElement = document.createElement("div");
-      bookElement.className = "book";
-      if (!book.read) {
-        bookElement.classList.add("unread");
-      }
-
-      // Create the book title element
-      const titleElement = document.createElement("h3");
-      titleElement.textContent = book.title;
-      bookElement.appendChild(titleElement);
-
-      // Create the book author element
-      const authorElement = document.createElement("p");
-      authorElement.textContent = "By " + book.author;
-      bookElement.appendChild(authorElement);
-
-      // Create the book cover image element
-      const coverElement = document.createElement("img");
-      coverElement.src = book.cover;
-      bookElement.appendChild(coverElement);
-
-      // Append the book element to the container
-      booksContainer.appendChild(bookElement);
-    });
+books.forEach((book) => {
+  // Create the book element
+  const bookElement = document.createElement("div");
+  bookElement.className = "book";
+  if (!book.read) {
+    bookElement.classList.add("unread");
   }
 
-  renderBooks(); // Initial rendering
+  // Create the book title element
+  const titleElement = document.createElement("h3");
+  titleElement.textContent = book.title;
+  bookElement.appendChild(titleElement);
 
-  // Save the books data to local storage
-  function saveBooksToStorage() {
-    localStorage.setItem("books", JSON.stringify(booksData));
-  }
+  // Create the book author element
+  const authorElement = document.createElement("p");
+  authorElement.textContent = "By " + book.author;
+  bookElement.appendChild(authorElement);
 
-  // Update the book status and save changes
-  function updateBookStatus(index, read) {
-    booksData[index].read = read;
-    saveBooksToStorage();
-    renderBooks();
-  }
+  // Create the book cover image element
+  const coverElement = document.createElement("img");
+  coverElement.src = book.cover;
+  bookElement.appendChild(coverElement);
 
-  // Fetch books data from the external JSON file
-  fetch("books.json")
-    .then((response) => response.json())
-    .then((data) => {
-      booksData.push(...data);
-      saveBooksToStorage();
-      renderBooks();
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+  // Append the book element to the container
+  booksContainer.appendChild(bookElement);
 });
